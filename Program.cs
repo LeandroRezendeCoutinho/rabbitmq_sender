61c5163332da540bc710e38ca575df30387f7c38
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-10-23 18:16:55 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rabbitmq_sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
